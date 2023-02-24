28fa6e31b8f7e8d56845cca8b10bc599afae5f7e
douglasgloverUCF <douglasglover3@knights.ucf.edu>
2023-02-23 22:34:02 -0500

    Improved Problem one capabilities

--- ProblemOne.java
+++ ProblemOne.java
@@ -1,28 +1,32 @@
 
 class GuestThread extends Thread {
     boolean insideLabyrinth = false;
-    boolean party = false;
+    boolean party = true;
     boolean hungry = true;
-    Cupcake cupcake;
     Labyrinth labyrinth;
     synchronized public void run()
     {
         System.out.println("Guest " + this.threadId() + " arrived to party.");
-        party = true;
         while(party)
         {
+            try {
+                sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             if(insideLabyrinth) {
                 System.out.println("Guest " + this.threadId() + " entered labyrinth.");
                 if(hungry) //guest has not eaten cupcake
                 {
-                    if(cupcake.eaten) // cupcake is absent
+                    if(labyrinth.cupcake.eaten) // cupcake is absent
                     {
-                        cupcake.request();
-                        cupcake.eaten = false;
+                        labyrinth.cupcake.request();
+                        labyrinth.cupcake.eaten = false;
+                        System.out.println("Cupcake requested.");
                     }
-                    if(!cupcake.eaten) //cupcake is present
+                    if(!labyrinth.cupcake.eaten) //cupcake is present
                     {
-                        cupcake.eaten = true;
+                        labyrinth.cupcake.eaten = true;
                         hungry = false;
                     }
                 }
@@ -35,19 +39,34 @@ class GuestThread extends Thread {
 }
 class Cupcake {
     public boolean eaten = false;
-    int requests = 0;
+    public int requests = 0;
     public void request(){
         requests++;
-        System.out.println("Cupcake requested.");
     }
 }
 class Labyrinth {
     public boolean full = false;
+    public GuestThread guests[];
+    Cupcake cupcake = new Cupcake();
+    public int n;
     public void enter(){
-        full = true;
+        int randomGuest = (int) Math.floor(Math.random() * n);
+        System.out.println("Random guest is " + guests[randomGuest].threadId());
+        guests[randomGuest].insideLabyrinth = true;
     }
     public void exit(){
-        full = false;
+        if(cupcake.requests >= n - 1)
+        {
+            //end the party
+            for (int i = 0; i < n; i++) { 
+                guests[i].party = false;
+            }
+
+            System.out.println("Party ended with " + cupcake.requests + " new cupcakes requested, therefore all " + n + " guests ate a cupcake in the labyrinth.");
+            System.exit(0);
+        }
+        else //get new guest
+            enter();
     }
 }
 
@@ -55,44 +74,20 @@ public class ProblemOne extends Thread{
     public static void main(String[] args)
     {
         //Number of guests
-        int n = 5;
+        int n = 100;
 
         GuestThread guests[] = new GuestThread[n];
-        Cupcake labyrinthCupcake = new Cupcake();
         Labyrinth labyrinth = new Labyrinth();
+        labyrinth.guests = guests;
+        labyrinth.n = n;
 
         for (int i = 0; i < n; i++) { 
             guests[i] = new GuestThread();
             guests[i].insideLabyrinth = false;
-            guests[i].cupcake = labyrinthCupcake;
             guests[i].labyrinth = labyrinth;
             guests[i].start();
         }
-        int randomGuest;
-        while(labyrinthCupcake.requests < n - 1) {
-            if(!labyrinth.full)
-            {
-                randomGuest = (int) Math.floor(Math.random() * n);
-                System.out.println("Random guest is " + guests[randomGuest].threadId());
-                guests[randomGuest].insideLabyrinth = true;
-                labyrinth.enter();
-            }
-        }
-        //end the party
-        for (int i = 0; i < n; i++) { 
-            guests[i].party = false;
-        }
 
-        System.out.println("Party ended with " + labyrinthCupcake.requests + " new cupcakes requested, therefore all " + n + " guests ate a cupcake in the labyrinth.");
-        //wait for each thread to finish
-        for (int i = 0; i < n; i++) { 
-            try {
-                guests[i].party = false;
-                guests[i].join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        labyrinth.enter();
     }
-    
 }
